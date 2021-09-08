316029d1ed00c2fc815582694a20d9d40d03886a
azaderdogan <nihatazad56@gmail.com>
2021-09-08 15:07:05 +0300

    rehber data changed

--- lib/models/rehber_model.dart
+++ lib/models/rehber_model.dart
@@ -64,9 +64,9 @@ class Rehber {
 
 List rehberList = [
   Rehber(
-    'Azad',
-    'https://pbs.twimg.com/profile_images/1422304925044854784/UpTPdQSo_400x400.jpg',
-    'Yazılım Mühendisi',
+    'Malik Budak',
+    'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg',
+    'I was born in 1993 in Istanbul. I graduated from Boğaziçi University, Department of History in 2018. I have been working as a tourist guide with a cockpit since January 2021. I have traveled many times abroad and especially in Europe. I have fluent English speaking skills. Intermediate German and...',
     10.0,
     [Language(flagUrl: 'bayrak_url', language: 'Türkçe')],
     [Tour('Night Tour', 230, '21.00', '04:00')],
@@ -76,9 +76,9 @@ List rehberList = [
     true,
   ),
   Rehber(
-      'Mirsaid',
+      'Afşın Dalgıç',
       'https://images.pexels.com/photos/775358/pexels-photo-775358.jpeg',
-      'Yazılım Mühendisi',
+      'My name is Afşın Dalgıç and I was born on Jan 20th. I grew up in one of the most beautiful cities in Turkey: Istanbul, historically also called Smyrna. Since the early childhood I always enjoyed travelling and exploring the ancient civilisations all around Turkey and the world. I studied English and...',
       10.0,
       [Language(flagUrl: 'bayrak_url', language: 'Türkçe')],
       [Tour('Night Tour', 230, '21.00', '04:00')],
@@ -87,9 +87,20 @@ List rehberList = [
       true,
       true),
   Rehber(
-      'Hatice Nur',
-      'https://pbs.twimg.com/profile_images/1422304925044854784/UpTPdQSo_400x400.jpg',
-      'Yazılım Mühendisi',
+      'Lale Smith',
+      'https://organicthemes.com/demo/profile/files/2018/05/profile-pic.jpg',
+      "I have returned to Turkey after living in the UK for 10 years. I have many years working in the retail industry and also ran my own successful beauty therapy business. I have lots of experience interacting with customers and I'm fluent in english, farsi, Azeri and Turkish. I would now like to...",
+      10.0,
+      [Language(flagUrl: 'bayrak_url', language: 'Türkçe')],
+      [Tour('Night Tour', 230, '21.00', '04:00')],
+      '5416744984',
+      'azadderdogan@gmail.com',
+      true,
+      true),
+  Rehber(
+      'Uluç Ali Fidan',
+      'https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg',
+      "Hi! I am a professional tour guide. (English/Turkish). I'm living in Istanbul. What I offer: -Private tour in Izmir, Ephesus, Pergamon -Private tour in Istanbul -Private tour in Denizli, Pamukkale we can also work on the destinations together for your planned trip to Turkey.",
       10.0,
       [Language(flagUrl: 'bayrak_url', language: 'Türkçe')],
       [Tour('Night Tour', 230, '21.00', '04:00')],
